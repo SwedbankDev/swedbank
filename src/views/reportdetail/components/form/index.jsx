c0b47aad6b8c8e11941f668361dfe08cb68a6555
Tim Phan <timphan92@gmail.com>
2020-01-24 12:29:00 +0100

    Added layout for reportdetail

--- src/views/reportdetail/components/form/index.jsx
+++ src/views/reportdetail/components/form/index.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import googleMap from '../../../../images/googleMapsImage.png';
+import chat from '../../../../images/chat.png';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 65% 35%;
+`;
+
+const ColumnWrapper = styled.div.attrs(props => ({
+  marginleft: props.marginLeft || 0
+}))`
+  display: flex;
+  flex-direction: column;
+  margin-left: ${props => props.marginleft};
+`;
+
+const Img = styled.img`
+  max-width: 300px;
+  max-height: 300px;
+  margin-bottom: 1rem;
+`;
 
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
-        <label htmlFor="category">Category:</label>
-        <div>Text</div>
-        <label htmlFor="Details">Details:</label>
-        <div>Lorem ipsum</div>
-      </div>
+      <Container>
+        <ColumnWrapper marginLeft={'10%'}>
+          <label htmlFor='category'>Category:</label>
+          <div>Text</div>
+          <label htmlFor='Details'>Details:</label>
+          <div>Lorem ipsum</div>
+        </ColumnWrapper>
+        <ColumnWrapper>
+          <Img src={googleMap} alt='gm'></Img>
+          <Img src={chat} alt='chat'></Img>
+        </ColumnWrapper>
+      </Container>
     );
   }
 }
--- src/views/reportdetail/index.js
+++ src/views/reportdetail/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import Button from '../../components/button';
 import Form from './components/form';
+import styled from 'styled-components';
+import googleMap from '../../images/googleMapsImage.png';
+
+const Container = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 20% auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
 export default class ReportDetail extends React.Component {
   render() {
+    console.log(this.props);
+
     return (
-      <h2>
-        ReportDetails <Button text={'button'} />
+      <Container>
+        <Header>
+          <h2>
+            Report{' '}
+            {this.props.match.params.id && !isNaN(this.props.match.params.id)
+              ? `#${this.props.match.params.id}`
+              : ''}
+          </h2>
+        </Header>
         <Form />
-      </h2>
+      </Container>
     );
   }
 }
